1c5079baca4bd967df0fe37f25d65f949fa76812
fshakoor <farhan.shakoor@coeus-solutions.de>
2022-06-23 00:28:21 +0500

    Build back-end application

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends \Symfony\Bundle\FrameworkBundle\Controller\AbstractController
+{
+    /**
+     * @Route("/{reactRouting}", name="home", defaults={"reactRouting": null})
+     */
+    public function index()
+    {
+        return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/api/users", name="users")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getUsers(ManagerRegistry $doctrine)
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $users = $repository->findAll();
+        $arr = array();
+        foreach ($users as $user){
+            $temp = array(
+                'id' => $user->getId(),
+                'gender' => $user->getGender(),
+                'firstName' => $user->getFirstName(),
+                'lastName' => $user->getLastName(),
+                'titleName' => $user->getTitleName(),
+                'street' => $user->getStreet(),
+                'city' => $user->getCity(),
+                'state' => $user->getState(),
+                'postalCode' => $user->getPostalCode(),
+                'email' => $user->getEmail(),
+                'phone' => $user->getPhone(),
+                'pictureUrl' => $user->getPictureUrl()
+            );
+            $arr[]=$temp;
+        }
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'application/json');
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->setContent(json_encode($arr));
+
+        return $response;
+    }
+
+    /**
+     *
+     * @Route("/api/user", name="get_user")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function getUserById(Request $request, ManagerRegistry $doctrine)
+    {
+        $repository = $doctrine->getRepository(User::class);
+        $response = new Response();
+        $id = $request->query->get('id');
+        $user = $repository->findOneBy(['id'=>$id]);
+        $arr = array(
+            'id' => $user->getId(),
+            'gender' => $user->getGender(),
+            'firstName' => $user->getFirstName(),
+            'lastName' => $user->getLastName(),
+            'titleName' => $user->getTitleName(),
+            'street' => $user->getStreet(),
+            'city' => $user->getCity(),
+            'state' => $user->getState(),
+            'postalCode' => $user->getPostalCode(),
+            'email' => $user->getEmail(),
+            'phone' => $user->getPhone(),
+            'pictureUrl' => $user->getPictureUrl()
+        );
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode($arr));
+        return $response;
+    }
+
+    /**
+     *
+     * @Route("/api/updateUser", name="update_user")
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function updateUser(Request $request, ManagerRegistry $doctrine){
+        $repository = $doctrine->getRepository(User::class);
+        $parameters = json_decode($request->getContent(), true);
+        $id = $parameters['id'];
+        $user = $repository->findOneBy(['id' => $id]);
+
+        $user->setGender($parameters['gender']);
+        $user->setCity($parameters['city']);
+        $user->setEmail($parameters['email']);
+        $user->setPhone($parameters['phone']);
+        $user->setPostalCode($parameters['postalCode']);
+        $user->setStreet($parameters['street']);
+        $user->setTitleName($parameters['titleName']);
+        $user->setFirstName($parameters['firstName']);
+        $user->setLastName($parameters['lastName']);
+
+        $em = $doctrine->getManager();
+        $em->flush();
+        $em->clear();
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/json');
+        $response->setContent(json_encode(['updated' => true]));
+        return $response;
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,288 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Table()
+ * @ORM\Entity()
+ */
+class User
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=7)
+     */
+    private $gender;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lastName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $titleName;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $street;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=100)
+     */
+    private $city;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=50)
+     */
+    private $state;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=20)
+     */
+    private $postalCode;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=50)
+     */
+    private $phone;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getGender(): string
+    {
+        return $this->gender;
+    }
+
+    /**
+     * @param string $gender
+     */
+    public function setGender(string $gender): void
+    {
+        $this->gender = $gender;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitleName(): string
+    {
+        return $this->titleName;
+    }
+
+    /**
+     * @param string $titleName
+     */
+    public function setTitleName(string $titleName): void
+    {
+        $this->titleName = $titleName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStreet(): string
+    {
+        return $this->street;
+    }
+
+    /**
+     * @param string $street
+     */
+    public function setStreet(string $street): void
+    {
+        $this->street = $street;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCity(): string
+    {
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     */
+    public function setCity(string $city): void
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * @return string
+     */
+    public function getState(): string
+    {
+        return $this->state;
+    }
+
+    /**
+     * @param string $state
+     */
+    public function setState(string $state): void
+    {
+        $this->state = $state;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostalCode(): string
+    {
+        return $this->postalCode;
+    }
+
+    /**
+     * @param string $postalCode
+     */
+    public function setPostalCode(string $postalCode): void
+    {
+        $this->postalCode = $postalCode;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPictureUrl(): string
+    {
+        return $this->pictureUrl;
+    }
+
+    /**
+     * @param string $pictureUrl
+     */
+    public function setPictureUrl(string $pictureUrl): void
+    {
+        $this->pictureUrl = $pictureUrl;
+    }
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    private $pictureUrl;
+}
